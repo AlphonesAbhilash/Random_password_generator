01d4aa52d3c0263fdd974ed2cd0f5c36d9ff29dd
AlphonesAbhilash <alphonesabhilash@gmail.com>
2024-08-10 10:49:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// script.js
+document.getElementById('generate').addEventListener('click', function() {
+    const length = document.getElementById('length').value;
+    const includeUppercase = document.getElementById('include-uppercase').checked;
+    const includeNumbers = document.getElementById('include-numbers').checked;
+    const includeSymbols = document.getElementById('include-symbols').checked;
+    
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    let characters = lowercase;
+    if (includeUppercase) characters += uppercase;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    document.getElementById('password').value = password;
+});
